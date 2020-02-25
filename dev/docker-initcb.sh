2fcb68b78f3f276fd45083047463590eac3fa4be
Support <support@bdverse.com>
2020-02-25 12:08:17 +0100

    Add init cb

---
+++ dev/docker-initcb.sh
@@ -0,0 +1,27 @@
+docker exec couchbase \
+/opt/couchbase/bin/couchbase-cli cluster-init \
+--cluster localhost
+--cluster-username Administrator \
+--cluster-password password \
+--cluster-ramsize 1280 \
+--cluster-name BDS \
+--cluster-index-ramsize 512 \
+--cluster-analytics-ramsize 512 \
+--index-storage-setting memopt \
+--services data,index,query,analytics
+
+docker exec couchbase \
+/opt/couchbase/bin/couchbase-cli bucket-create \
+--cluster localhost \
+--username Administrator \
+--password password \
+--bucket BDS \
+--bucket-ramsize 256
+
+docker exec couchbase \
+/opt/couchbase/bin/couchbase-cli bucket-create \
+--cluster localhost \
+--username Administrator \
+--password password \
+--bucket VAR \
+--bucket-ramsize 1024
