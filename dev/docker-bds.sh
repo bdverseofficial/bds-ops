f8a304f3de29aacbbbd0edc9ec5456967b4babea
Support BDVerse SA <support@bdverse.com>
2020-05-14 13:39:16 +0200

    Update to beta 743

--- dev/docker-bds.sh
+++ dev/docker-bds.sh
@@ -2,15 +2,15 @@ docker stop bds
 docker rm bds
 
 mkdir -p ~/bds/server/certificates
-
 mkdir -p ~/bds/server/plugins
-
 mkdir -p ~/bds/server/data
+mkdir -p ~/bds/html/cms
+mkdir -p ~/bds/html/assets
 
 mkdir -p ~/bds/server/config
 cp ./bds-config/global.conf.json ~/bds/server/config/
 
-docker pull bdverseofficial/bds:dev
+docker pull bdverseofficial/bds
 
 docker run -d --restart unless-stopped --name bds \
 -p 49080:49080 \
@@ -20,4 +20,6 @@ docker run -d --restart unless-stopped --name bds \
 -v ~/bds/server/certificates:/app/certificates \
 -v ~/bds/server/plugins:/app/thirdpartyplugins \
 -v ~/bds/server/data:/app/thirdpartydata \
-bdverseofficial/bds:dev
+-v ~/bds/html/assets:/app/assets \
+-v ~/bds/html/cms:/app/cms \
+bdverseofficial/bds
